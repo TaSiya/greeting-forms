3c34153ac1ec836f5f86cea21adea58f4981a966
tasiya <siyandamakhalima.1@gmail.com>
2018-08-15 12:59:39 +0200

    Allow the user to be added through the url. Language selection feature.

--- greet.js
+++ greet.js
@@ -1,31 +1,28 @@
-function Greeting(storedData){
+module.exports = function Greeting(){
     var namesGreeted = {};
-    var user = '';
-    var language = "";
+    var counter = 0 ;
   
-    function setName(value){ user = value; }
-  
-    function checking(){
-       if(storedData){
-          namesGreeted = storedData;
-       }
-       if(user !== ''){
-          if(namesGreeted[user] === undefined){
-            namesGreeted[user] = 0;
+    function checking(value){
+      let flag = false;
+       if(value !== ''){
+          if(namesGreeted[value] === undefined){
+            namesGreeted[value] =counter;
+            counter ++;
+            flag = true;
           }
        }
+       return flag;
     }
     //To get the length of the names greeted
     function countGreeted(){ return Object.keys(storedData).length; }
   
     function getMap(){ return namesGreeted ; }
-    function getName(){ return user; }
+    function getCount(){ return counter;}
   
     return {
       userMap : getMap,
       checked : checking,
       userLength :countGreeted,
-      setNames : setName,
-      users : getName
+      getCount
     }
   }
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const handle = require('express-handlebars');
 const bodyParser = require('body-parser');
 const greet = require('./greet');
 
+var Igreet = greet();
 const app = express();
 
 app.use(express.static('public'));
@@ -16,20 +17,33 @@ app.engine('handlebars', handle({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 app.get('/', function(req, res){
-    
-    res.render('home',);
+    let counter = Igreet.getCount();
+    res.render('home', {counter});
 })
 
-app.get('/greetings', function(req, res){
-    let name = req.params.input;
-    console.log(name);
-    let message = "Good day";
+app.post('/greetings/', function(req, res){
+    let input = req.body.name;
+    let language = req.body.languageSelect;
+    let flag = Igreet.checked(input);
+    let counter = Igreet.getCount();
+    if(language === undefined){
+        flag = false;
+    }
+    let message = language +", "+ input;
+    let namePlease = "Please enter your name and select a language";
+    res.render('home',{flag,message,namePlease, counter});
+});
 
-    res.render('home', {message});
+app.get('/greetings/:name', function(req, res){
+    let input = req.params.name;
+    let flag = Igreet.checked(input);
+    let message = "Good day "+ input;
+    let counter = Igreet.getCount();
+    res.render('home',{flag, input, message, counter});
 })
+
 
 const PORT = process.env.PORT || 2018;
-
 app.listen(PORT, function(){
     console.log('Listening to port....'+PORT);
 });
