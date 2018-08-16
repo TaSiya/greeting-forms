84d5d05d9a77938196023fecfc5d5e747786ccdb
tasiya <siyandamakhalima.1@gmail.com>
2018-08-16 12:08:34 +0200

    some changes in the home template

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const flash = require('express-flash');
+const session = require('express-session');
 const handle = require('express-handlebars');
 const bodyParser = require('body-parser');
 const greet = require('./greet');
@@ -13,6 +15,15 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json());
 
+app.use(session({
+    secret : "<add a secret string here>",
+    resave: false,
+    saveUninitialized: true
+  }));
+
+// initialise the flash middleware
+app.use(flash());
+
 app.engine('handlebars', handle({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
@@ -21,13 +32,13 @@ app.get('/', function(req, res){
     res.render('home', {counter});
 })
 
+// Greet the user using the form field
 app.post('/greetings/', function(req, res){
     let input = req.body.name;
     let language = req.body.languageSelect;
     
     if(language !== undefined){
         var flag = Igreet.checked(input);
-        
     }
     var counter = Igreet.getCount();
     let message = language +", "+ input;
@@ -35,6 +46,7 @@ app.post('/greetings/', function(req, res){
     res.render('home',{flag,message,namePlease, counter});
 });
 
+// Allows the user to be greeted using the URL. But the language is not set, so i use default "Good day"
 app.get('/greetings/:name', function(req, res){
     let input = req.params.name;
     let flag = Igreet.checked(input);
@@ -42,6 +54,11 @@ app.get('/greetings/:name', function(req, res){
     let counter = Igreet.getCount();
     res.render('home',{flag, input, message, counter});
 })
+
+app.get('/greetings', function (req, res) {
+    req.flash('info', 'Flash Message Added');
+    res.redirect('/');
+});
 
 
 const PORT = process.env.PORT || 2018;
