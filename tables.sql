e381d94e8eb1c4f51dac7e6b559fb1d125d6301f
tasiya <siyandamakhalima.1@gmail.com>
2018-08-31 16:38:50 +0200

    added another column in template

--- tables.sql
+++ tables.sql
@@ -3,6 +3,7 @@ drop table if EXISTS users;
 create table users(
     id serial not null primary key,
     users_greeted varchar(100) not null,
-    user_language text not null
+    user_language text not null,
+    counter int not null
 );
 
